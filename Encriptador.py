eb4e194c8921e9befc58776b0b3981a2a8b82517
Santino Cardelli <sancardelli@gmail.com>
2024-04-19 11:44:51 -0300

    Funcion Main

--- Encriptador.py
+++ Encriptador.py
@@ -1,16 +1,5 @@
 from PIL import Image
 import numpy as np
-dic_encriptacion = {
-    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
-    'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19,
-    't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, ' ': 27, ',': 28,
-    '?': 29, '!': 30, '¿': 31, '¡': 32, '(': 33, ')': 34, ':': 35, ';': 36, '-': 37,
-    '“': 38, '‘': 39, 'á': 40, 'é': 41, 'í': 42, 'ó': 43, 'ú': 44, 'ü': 45, 'ñ': 46
-}
-imagen = Image.open("baboon.png")
-imagen_array = np.array(imagen)
-tamaño_imagen_original = len(imagen_array)
-print(tamaño_imagen_original)
 
 def agregar_marco(imagen_array):
     """
@@ -39,7 +28,7 @@ def aplicar_filtro(tamaño_imagen_original, imagen_array):
         for j in range(2, tamaño_imagen_original):
             imagen_array[i-2][j-2] = pixel_suavisado(imagen_array_marco, i, j)
 
-def encriptado(mensaje): 
+def encriptado(mensaje,dic_encriptacion): 
     """
     Funcion que encripta un mensaje en base a un diccionario de encriptacion
     Args:
@@ -59,9 +48,23 @@ def encriptado(mensaje):
             mensaje_encriptado.append(-1)
     mensaje_encriptado.append(0)     
     return mensaje_encriptado
+def main():
+    dic_encriptacion = {
+    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
+    'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19,
+    't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, ' ': 27, ',': 28,
+    '?': 29, '!': 30, '¿': 31, '¡': 32, '(': 33, ')': 34, ':': 35, ';': 36, '-': 37,
+    '“': 38, '‘': 39, 'á': 40, 'é': 41, 'í': 42, 'ó': 43, 'ú': 44, 'ü': 45, 'ñ': 46
+    }
+    imagen = Image.open("baboon.png")
+    imagen_array = np.array(imagen)
+    tamaño_imagen_original = len(imagen_array)
+    print(tamaño_imagen_original)
+    pixel_suavisado(agregar_marco(imagen_array), 2, 2)
+    # imagen_array = agregar_marco(imagen_array)
+    # print(imagen_array.shape)
+    # imagen = Image.fromarray(imagen_array)
+    # imagen.save("baboon_marco.png")
 
-pixel_suavisado(agregar_marco(imagen_array), 2, 2)
-# imagen_array = agregar_marco(imagen_array)
-# print(imagen_array.shape)
-# imagen = Image.fromarray(imagen_array)
-# imagen.save("baboon_marco.png")
+if __name__ == "__main__":
+    main()
